2e306ae60e6d1221129904e99f17dc42d379f9f6
Kobi Jones <kobijonesmail@gmail.com>
2022-04-13 22:55:55 -0500

    Removed standard lib stuff

--- src/Bag.cpp
+++ src/Bag.cpp
@@ -1,36 +1,21 @@
 #include "Bag.hpp"
 
-// #ifdef USE_STANDARD_LIBRARY
+Bag::Bag() : m_size(0), m_items(std::unique_ptr<std::string[]>()) {}
 
-void Bag::add(const std::string &item) { m_items.push_back(item); }
-
-void Bag::display() {
-  std::cout << "Items in bag:" << std::endl;
-  for (const auto &item : m_items) {
-    std::cout << '\t' << item << std::endl;
+void Bag::add(const std::string &item) {
+  std::unique_ptr<std::string[]> newArray =
+      std::unique_ptr<std::string[]>(new std::string[m_size + 1]);
+  for (size_t i = 0; i < m_size; i++) {
+    newArray[i] = m_items[i];
   }
+  newArray[m_size++] = item;
+  m_items = std::move(newArray);
 }
 
-// #else
-
-// Bag::Bag() : m_size(0), m_items(std::unique_ptr<std::string[]>()) {}
-
-// void Bag::add(const std::string &item) {
-//   std::unique_ptr<std::string[]> newArray =
-//       std::unique_ptr<std::string[]>(new std::string[m_size + 1]);
-//   for (size_t i = 0; i < m_size; i++) {
-//     newArray[i] = m_items[i];
-//   }
-//   newArray[m_size++] = item;
-//   m_items = std::move(newArray);
-// }
-
-// void Bag::display() {
-//   std::cout << "Items in bag: " << std::endl;
-//   std::cout << m_items.get() << std::endl;
-//   for (size_t i = 0; i < m_size; i++) {
-//     std::cout << '\t' << m_items[i] << std::endl;
-//   }
-// }
-
-// #endif
+void Bag::display() {
+  std::cout << "Items in bag: " << std::endl;
+  std::cout << m_items.get() << std::endl;
+  for (size_t i = 0; i < m_size; i++) {
+    std::cout << '\t' << m_items[i] << std::endl;
+  }
+}
